9ff9d952d1ac23335dfbaca35a82ce16dbc65055
Joseph Elias <jrelias@outlook.com>
2022-04-19 14:32:02 -0400

    pre order, post order, and in order traversal

---
+++ AlgoExpert/prePostInOrderTraverse.js
@@ -0,0 +1,37 @@
+// Time: O(n)
+//Space: O(n)
+
+function inOrderTraverse(tree, array) {
+  // Write your code here.
+  if (tree !== null) {
+    inOrderTraverse(tree.left, array);
+    array.push(tree.value);
+    inOrderTraverse(tree.right, array);
+  }
+  return array;
+}
+
+function preOrderTraverse(tree, array) {
+  // Write your code here.
+  if (tree !== null) {
+    array.push(tree.value);
+    preOrderTraverse(tree.left, array);
+    preOrderTraverse(tree.right, array);
+  }
+  return array;
+}
+
+function postOrderTraverse(tree, array) {
+  if (tree !== null) {
+    postOrderTraverse(tree.left, array);
+    postOrderTraverse(tree.right, array);
+    array.push(tree.value);
+  }
+  return array;
+  // Write your code here.
+}
+
+// Do not edit the lines below.
+exports.inOrderTraverse = inOrderTraverse;
+exports.preOrderTraverse = preOrderTraverse;
+exports.postOrderTraverse = postOrderTraverse;
