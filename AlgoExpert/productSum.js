d250bdabe626058147e73cd063da9e803c1b9358
Joseph Elias <jrelias@outlook.com>
2022-04-22 16:38:35 -0400

    optimized

--- AlgoExpert/productSum.js
+++ AlgoExpert/productSum.js
@@ -1,5 +1,26 @@
 // Tip: You can use the Array.isArray function to check whether an item
 // is a list or an integer.
+
+// Time: O(n) where n is the total sum of all elements in array including sub arrays
+
+function productSum(array, multiplier = 1) {
+  // Write your code here.
+  let sum = 0;
+  for (let item of array) {
+    if (Array.isArray(item)) {
+      sum += productSum(item, multiplier + 1);
+    } else {
+      sum += item;
+    }
+  }
+  return sum * multiplier;
+}
+
+// Do not edit the line below.
+exports.productSum = productSum;
+
+// Tip: You can use the Array.isArray function to check whether an item
+// is a list or an integer.
 function productSum(array) {
   // Write your code here.
   let depth = 1;
